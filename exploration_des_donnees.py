4e1359bb795966582425f79157a3d879155bb978
Baptiste Dr <baptiste.de-rigaud@student-cs.fr>
2025-12-27 19:52:15 +0100

    étape 2

---
+++ exploration_des_donnees.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns   
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+# Voir les 5 premières lignes
+print("en 1 ", train.head())
+
+# Nombre de lignes et colonnes
+print("en 2 ",train.shape)
+
+# Liste des colonnes et types
+print("en 3 ",train.dtypes)
+
+# Statistiques descriptives pour les numériques
+print("en 4 ",train.describe())
+
+# Nombre de NaN par colonne
+print("en 5 ", train.isnull().sum().sort_values(ascending=False))
+
+# Numériques
+num_features = train.select_dtypes(include=['int64', 'float64']).columns
+print("Numériques :", num_features)
+
+# Catégorielles
+cat_features = train.select_dtypes(include=['object']).columns
+print("Catégorielles :", cat_features)
+
+# Aperçu statistique
+print(train['SalePrice'].describe())
+
+# Histogramme
+sns.histplot(train['SalePrice'], kde=True)
+sns.histplot(train['LotArea'], kde=True)
+plt.show()
+
+# Boxplot pour voir les outliers
+sns.boxplot(x=train['SalePrice'])
+sns.boxplot(x=train['LotArea'])
+plt.show()
